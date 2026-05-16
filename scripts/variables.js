0a68073fff5fb041e3f728577e9763584f40191d
push-and-pray-coder <Titan.Chen@sdgku.edu>
2026-05-16 11:54:09 -0700

    Variables

---
+++ scripts/variables.js
@@ -0,0 +1,47 @@
+console.log("====================")
+console.log("Variables Script");
+console.log("====================")
+
+//string (text) ""
+let firstName = "Titan"
+//numerical 34,67,3.0
+let age = 99;
+//boolean (true/false)
+let isStudent = false;
+let isProfessor= true;
+
+console.log(firstName);
+console.log(age);
+console.log(isStudent);
+
+
+//Mini Challenge:
+let lastName = "Chen";
+let height = "67";
+
+console.log(lastName);
+console.log(height);
+
+let num1 = 10;
+let num2 = 20;
+
+let sum = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+
+//concatenation (join strings)
+
+//string interpolation
+console.log(`${num1} + ${num2} = ${sum}`)
+console.log(`${num1} - ${num2} = ${sub}`)
+console.log(`${num1} * ${num2} = ${mult}`)
+console.log(`${num1} / ${num2} = ${div}`)
+
+//radius of a circle
+let area;
+let pi = 3.14;
+let radius  = 3;
+area = pi * radius**2;
+
+console.log  (`$The area of a circle with a raidus of ${radius} is the ${area} `);
